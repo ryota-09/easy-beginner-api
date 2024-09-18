9eba2a96c4a9a054474bd5cb33c9ff140eb48946
ryota-09 <n.ryo950911@gmail.com>
2024-09-18 19:49:06 +0900

    fix: loginエンドポイントの追加

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,35 +2,16 @@ import { corsHeaders } from "@/lib";
 import supabase from "@/lib/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
-  const id = params.id;
-  const { data, error } = await supabase.from('users').select('*').eq('id', id).single();
-
-  if (error) {
-    return NextResponse.json({ status: 500, message: "エラーが発生しました。" })
-  }
-
-  return await NextResponse.json({ status: 200, data: data }, {
-    status: 200,
-    headers: corsHeaders
-  })
-}
-
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+export async function POST(req: NextRequest) {
   const apiKey = req.headers.get('X-API-KEY');
   if (apiKey !== process.env.NEXT_PUBLIC_API_KEY) {
     return NextResponse.json({ status: 401, message: "APIキーが無効です。" })
   }
 
-  const id = params.id;
   const body = await req.json();
-  const { userName, email, password } = body;
+  const { email, password } = body;
 
-  const { data, error } = await supabase.from('users').update({
-    userName: userName,
-    email: email,
-    password: password
-  }).eq('id', id);
+  const { data, error } = await supabase.from('users').select('*').eq('email', email).eq('password', password).single();
 
   if (error) {
     return NextResponse.json({ status: 500, message: "エラーが発生しました。" })
